e08809002745898b44f3a34c21d621e185863541
Rohan Neure <114942902+neurerohan@users.noreply.github.com>
2025-03-07 16:13:42 +0545

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with default config
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://kalimatirate.nyure.com.np',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://api.kalimatirate.nyure.com.np',
   headers: {
     'Content-Type': 'application/json',
   },
