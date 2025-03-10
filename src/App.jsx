4776f89cc27c69280126406f6a8a0fce5c2899bd
neurerohan <[nyureron@gmail.com]>
2025-03-10 23:39:45 +0545

    gooo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import KalimatiPrices from './pages/KalimatiPrices';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 const App = () => {
@@ -9,7 +10,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/kalimati-tarkari-rate-today/" element={<KalimatiPrices />} />
+        <Route path="/kalimati-tarkari-rate-today" element={<KalimatiPrices />} />
         {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
         <Route path="*" element={<NotFound />} />
       </Routes>
